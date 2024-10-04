028965a3d6bccfde75e35323e6737855dfa57d7d
Marvina Young <168630228+marvinayoung@users.noreply.github.com>
2024-10-04 09:07:51 +0700

    Add Project file

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.title("Data Analysis Dashboard")
+
+hours_df = pd.read_csv("hour.csv")
+hours_group = hours_df.groupby(by="hr").cnt.mean()
+days_df = pd.read_csv("day.csv")
+
+st.subheader("Data Preview - hour")
+st.write(hours_df.head())
+    
+st.subheader("Data Preview - day")
+st.write(days_df.head())
+
+st.subheader("Visualisasi Data")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.write("Tren Rata-rata Jumlah Penyewaan Sepeda Setiap Jam")
+    plt.plot(hours_group.index, hours_group.values)
+    plt.xlabel("Jam")
+    plt.ylabel("Jumlah")
+    plt.xlim(0,24)
+    st.pyplot(plt)
+    with st.expander("See explanation"):
+        st.write(
+            """Dari grafik menunjukkan pengguna sepeda memuncak di jam berangkat 
+            kerja sekitar jam 8 pagi dan memuncak kembali di jam pulang kerja 
+            sekitar jam 5 sore.
+            """
+        )
+with col2:
+    st.write("Korelasi antara suhu dan jumlah penyewaan sepeda")
+    sns.regplot(x=days_df["temp"], y=days_df["cnt"])
+    plt.xlim(0, 1)
+    st.pyplot(plt)
+    with st.expander("See explanation"):
+        st.write(
+            """Dari nilai korelasi menunjukkan 0.628844 lebih mendekati angka 1 
+            dibandingkan angka 0 dan dari grafik menunjukkan kenaikan, maka
+            disimpulkan suhu dan jumlah penyewaan sepeda memiliki korelasi positive
+            yang cukup erat.
+            """
+        )
---
+++ README.md
@@ -0,0 +1,22 @@
+# Bike-Sharing Dashboard ✨
+
+## Setup Environment - Anaconda
+```
+conda create --name main-ds python=3.12.4
+conda activate main-ds
+pip install -r requirements.txt
+```
+
+## Setup Environment - Shell/Terminal
+```
+mkdir Submission
+cd Submission
+pipenv install
+pipenv shell
+pip install -r requirements.txt
+```
+
+## Run steamlit app
+```
+streamlit run dashboard.py
+```
